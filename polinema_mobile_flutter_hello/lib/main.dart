41307402a3fbf793321c602ae8a4e85699d0249a
evanightly <adamhenderson3x3@gmail.com>
2023-09-16 20:49:57 +0700

    Bab 1

---
+++ polinema_mobile_flutter_hello/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Tugas Pertama Flutter'),
+      ),
+      body: const Center(
+        child: Text('Galur Arasy Lumintang\n2141762136'),
+      ),
+    ),
+  ));
+}
